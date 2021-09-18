d241e7d1e607c74c964b4abb3f202ce2c4697bad
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-09-18 20:14:27 +0530

    ADDED LINKS AND ICONS
    -added icons in footer
    -added external links in footer
    -added style for every external link

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Footer.css'
+import { ImLinkedin, ImMail } from 'react-icons/im'
+import { SiGithub, SiCodesandbox, SiReplDotIt } from 'react-icons/si'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
-        <footer>
-            
+        <footer className="footer">
+            <a href="https://www.linkedin.com/in/vidhi-thakur-9353271a8" target="_blank" rel="noreferrer">
+                <ImLinkedin className="icons" />
+            </a>
+            <a href="https://github.com/vidhi-thakur" target="_blank" rel="noreferrer">
+                <SiGithub className="icons" />
+            </a>
+            <a href="https://codesandbox.io/dashboard/home" target="_blank" rel="noreferrer">
+                <SiCodesandbox className="icons" />
+            </a>
+            <a href="https://replit.com/@vidhithakur/" target="_blank" rel="noreferrer">
+                <SiReplDotIt className="icons" />
+            </a>
+            <a href="mailto:smmthakur@gmail.com" target="_blank" rel="noreferrer">
+                <ImMail className="icons" />
+            </a>
+
         </footer>
     )
 }
