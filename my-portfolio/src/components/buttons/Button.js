17bd8181ced04b7eb48e2937c6a75ccf16e1b82a
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-09-19 14:45:31 +0530

    ADDED BUTTON COMPONENT
    -added custom button component
    -styled button component based on the type of button i.e., primary or secondary

---
+++ my-portfolio/src/components/buttons/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Button.css'
+
+function Button({ title, internalLink, externalLInk, type }) {
+    return (
+        <div>
+            {internalLink && <Link className="link" to={internalLink}>
+                <button className={`cp button ${type}`}>{title}</button>
+            </Link>}
+            {externalLInk && <a href={internalLink} target="_blank" rel="noreferrer">
+                <button className={`cp button ${type}`}>{title}</button>
+            </a>}
+        </div>
+    )
+}
+
+export default Button
